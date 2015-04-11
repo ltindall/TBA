3d9231ce389dfb73208d79f9f55e3cf42a777262
ltindall <ltindall@ucsd.edu>
2015-04-11 01:55:33 -0700

    2 am ... send help

--- com.cse110.tba/src/com/example/com/cse110/tba/MainActivity.java
+++ com.cse110.tba/src/com/example/com/cse110/tba/MainActivity.java
@@ -2,21 +2,27 @@ package com.example.com.cse110.tba;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
-
+import com.parse.ParseUser;
+import com.parse.ParseACL;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	ParseObject testObject = new ParseObject("UserClass");
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Parse.initialize(this, "9Sejg1tgXT9qRkeS3uqoykI9E84kVb8DAlCPzsNi", "MtMjrzfToRzCGf7mzFtnPXlApnUFBAqEGPmty8bm");
-		ParseObject testObject = new ParseObject("test");
-		//testObject.put("foo", "bar");
-		//testObject.saveInBackground();
+		Parse.enableLocalDatastore(this);
+		initializeParse("9Sejg1tgXT9qRkeS3uqoykI9E84kVb8DAlCPzsNi",
+				"MtMjrzfToRzCGf7mzFtnPXlApnUFBAqEGPmty8bm");
+
+		testObject.put("NameField", "Lamp");
+		testObject.saveInBackground();
+		///push this 
 	}
 
 	@Override
@@ -26,4 +32,10 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	private void initializeParse(String app_name, String client_id) {
+		Parse.initialize(this, app_name, client_id);
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+		ParseACL.setDefaultACL(defaultACL, false);
+	}
 }
