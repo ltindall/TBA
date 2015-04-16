eb4b309295810ec76d3a08e2637620e14d303d17
jblynch1 <jblynch@ucsd.edu>
2015-04-15 21:53:00 -0700

    The entire environment

---
+++ com.cse110.tba/src/com/example/com/cse110/tba/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.com.cse110.tba;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.ParseACL;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	ParseObject testObject = new ParseObject("UserClass");
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Parse.enableLocalDatastore(this);
+		initializeParse("9Sejg1tgXT9qRkeS3uqoykI9E84kVb8DAlCPzsNi",
+				"MtMjrzfToRzCGf7mzFtnPXlApnUFBAqEGPmty8bm");
+
+		testObject.put("NameField", "Lamp");
+		testObject.saveInBackground();
+		///push this 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	private void initializeParse(String app_name, String client_id) {
+		Parse.initialize(this, app_name, client_id);
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+		ParseACL.setDefaultACL(defaultACL, false);
+	}
+}
