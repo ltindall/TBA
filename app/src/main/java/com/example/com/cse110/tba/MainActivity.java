39cd341ee17bbd9a7accf728c586dc6f87c004b0
jblynch1 <jblynch@ucsd.edu>
2015-04-17 23:37:02 -0700

    Enabled anon user (untested)

--- app/src/main/java/com/example/com/cse110/tba/MainActivity.java
+++ app/src/main/java/com/example/com/cse110/tba/MainActivity.java
@@ -1,35 +1,51 @@
 package com.example.com.cse110.tba;
 
-import com.parse.ParseObject;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseUser;
 import com.parse.ui.ParseLoginBuilder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-	ParseObject testObject = new ParseObject("UserClass");
+    public static final int LOGIN_PAGE = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		testObject.put("NameField", "Lamp");
-        testObject.add("commit", null);
         ParseLoginBuilder builder = new ParseLoginBuilder(this);
-        startActivityForResult(builder.build(), 0);
-        //additional code here
-		//testObject.saveInBackground();
-		///push this
-        //extra crap
+        startActivityForResult(builder.build(), LOGIN_PAGE);
 	}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == LOGIN_PAGE)
+        {
+            if(resultCode == Activity.RESULT_OK)
+            {
+                Log.d("MainActivity", "User: " + ParseUser.getCurrentUser().getUsername() + " successfully authenticated");
+            }
+            else if(resultCode == Activity.RESULT_CANCELED)
+            {
+                ParseAnonymousUtils.logInInBackground();
+                Log.d("MainActivity", "Authentication Failed, Creating Anon user");
+            }
+        }
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+        menu.add("Buy");
+        menu.add("Sell");
 		return true;
 	}
 }
